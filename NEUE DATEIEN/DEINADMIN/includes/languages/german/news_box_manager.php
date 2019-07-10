a8424a8752f658fec3c82611b1acd4ae089011b9
webchills <office@webchills.com>
2019-07-10 16:37:38 +0200

    Version 2.2.1

---
+++ NEUE DATEIEN/DEINADMIN/includes/languages/german/news_box_manager.php
@@ -0,0 +1,45 @@
+<?php
+// -----
+// Part of the News Box Manager plugin, re-structured for Zen Cart v1.5.1 and later by lat9.
+// Copyright (C) 2015-2019, Vinos de Frutas Tropicales
+// Translation for Zen Cart German 2019-07-10 webchills
+//
+// +----------------------------------------------------------------------+
+// | Do Not Remove: Coded for Zen-Cart by geeks4u.com                     |
+// | Dedicated to Memory of Amelita "Emmy" Abordo Gelarderes              |
+// +----------------------------------------------------------------------+
+//
+define('NEWS_BOX_HEADING_TITLE', 'News Box Manager');
+
+define('TABLE_HEADING_NEWS', 'News');
+define('TABLE_HEADING_NEWS_START', 'Startdatum');
+define('TABLE_HEADING_NEWS_END', 'Enddatum');
+define('TABLE_HEADING_MODIFIED', 'zuletzt geändert');
+define('TABLE_HEADING_STATUS', 'Status');
+define('TABLE_HEADING_ACTION', 'Aktion');
+
+define('TEXT_NEWS_TITLE', 'News Titel:');
+define('TEXT_NEWS_CONTENT', 'News Inhalt:');
+define('TEXT__NEWS_CONTENT', 'News Inhalt:');
+define('TEXT_NEWS_METATAGS_TITLE', 'Metatags Titel:');
+define('TEXT_NEWS_METATAGS_DESCRIPTION', 'Metatags Beschreibung:');
+define('TEXT_NEWS_METATAGS_KEYWORDS', 'Metatags Keywords:');
+
+define('TEXT_NEWS_METATAGS_DISCLAIMER', '<b>Note:</b> News articles\' metatags are available for installations using Zen Cart 1.5.5 or later!');
+define('TEXT_NEWS_DATE_ADDED', 'Hinzugefügt am:');
+define('TEXT_NEWS_DATE_MODIFIED', 'Geändert am:');
+define('TEXT_NEWS_START_DATE', 'News startet am:');
+define('TEXT_NEWS_END_DATE', 'News endet am:');
+
+
+define('TEXT_NEWS_DELETE_INFO', 'Wollen Sie diesen News Beitrag wirklich löschen?');
+
+define('ERROR_NEWS_TITLE_CONTENT', 'Der <em>News Titel</em> und <em>News Inhalt</em> müssen zumindest für eine Sprache ausgefüllt sein.');
+define('ERROR_NEWS_DATE_ISSUES', 'Das <em>Start Datum</em> muss vor dem <em>End Datum</em> liegen.');
+define('SUCCESS_NEWS_ARTICLE_CHANGED', 'Der News Beitrag wurde %s.');
+define('NEWS_ARTICLE_UPDATED', 'aktualisiert');
+define('NEWS_ARTICLE_CREATED', 'erzeugt');
+
+define('TEXT_DISPLAY_NUMBER_OF_NEWS', 'Zeige <b>%d</b> bis <b>%d</b> (von <b>%d</b> News)');
+define('TEXT_NEWS_BOX_MANAGER_INFO', 'Hier können Sie News Beiträge verfassen, die dann im Shop angezeigt werden. Diverse Einstellungen dazu stehen unter <em>Konfiguration-&gt;News Box Manager</em> zur Verfügung.<br /><br />Ein gültiger News Beitrag muss einen &quot;News Titel&quot; und &quot;News Inhalt&quot; in zumindest einer Ihrer im Shop aktiven Sprachen haben.');
+define('TEXT_EDIT_INSERT_INFO', 'Falls Sie das <em>Start Datum</em> leer lassen wird das heutige Datum verwendet.  Lassen Sie das <em>End Datum</em> leer für einen News Beitrag, der niemals abläuft.');
---
+++ NEUE DATEIEN/includes/languages/german/extra_definitions/news_box_manager_defines.php
@@ -0,0 +1,21 @@
+<?php
+// -----
+// Part of the News Box Manager plugin, re-structured for Zen Cart v1.5.1 and later by lat9.
+// Copyright (C) 2015, Vinos de Frutas Tropicales
+//
+// +----------------------------------------------------------------------+
+// | Do Not Remove: Coded for Zen-Cart by geeks4u.com                     |
+// | Dedicated to Memory of Amelita "Emmy" Abordo Gelarderes              |
+// +----------------------------------------------------------------------+
+//
+define ('BOX_HEADING_NEWS_BOX', 'Aktuelle News');
+define ('TEXT_LINK_MORE', 'mehr &hellip;');
+define ('TEXT_TRAIL_STR', '&hellip;');
+define ('TEXT_ALL_NEWS', '[zeige alle]');
+
+define ('NEWS_DATE_SEPARATOR', ' - ');
+
+define ('NEWS_BOX_HEADING_TITLE', 'Artikel Titel');
+define ('NEWS_BOX_HEADING_DATES', 'Artikel Datum');
+
+define ('TEXT_NEWS_BOX_INFO', 'Klicken Sie einen Artikel Titel an, um den Inhalt anzuzeigen oder klicken Sie auf <em>zeige alle</em> um alle aktuellen News von ' . STORE_NAME . ' anzuzeigen!');
